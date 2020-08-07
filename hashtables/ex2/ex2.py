c363589dcd19bf984de9e6d45e3aae61f894bcaf
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-08-07 09:15:09 -0700

    finished second problem

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    ht = {}
+    route = []
+    for ticket in tickets:
+        ht[ticket.source] = ticket.destination
+    print(ht)
+    route.append(ht['NONE'])
+    n = ht['NONE']
+    while ht[n] != 'NONE':
+        route.append(ht[n])
+        n = ht[n]
+    route.append('NONE')
+    print(route)
     return route
