a46ae07b6f1ee15f9a6f6df40189d1f086378df8
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-08-07 10:51:26 -0700

    finished fourth problem

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,7 +7,16 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    ht = {}
+    for q in queries:
+        if q not in ht:
+            ht[q] = []
+    for f in files:
+        g = f.split("/", -1)[-1]
+        if g in ht:
+            result.append(f)
+  
     return result
 
 
